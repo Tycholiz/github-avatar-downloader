6c9047d1687732ddefa391b43a137c6259006af0
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-07 00:33:07 +0000

    step 3

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,8 @@
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//Node-style callback functions expect their first argument to be a placeholder for any errors that may have occurred, and the subsequent argument(s) are results being passed to the callback.
+function getRepoContributors(repoOwner, repoName, cb) { //need callback to handle the asynchronous nature of results that are to be returned from getRepoContributors.
+
+
+}
