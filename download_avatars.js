22abb4615c63f1b0874e5a9f94c73b82bc90ec3a
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-07 02:20:01 +0000

    able to print avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,30 @@
 var request = require('request');
+var secrets = require('./secrets');
 console.log('Welcome to the GitHub Avatar Downloader!');
-
 //Node-style callback functions expect their first argument to be a placeholder for any errors that may have occurred, and the subsequent argument(s) are results being passed to the callback.
 function getRepoContributors(repoOwner, repoName, cb) { //need callback to handle the asynchronous nature of results that are to be returned from getRepoContributors.
+  var options = {
+    url:  "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body) {
+    // cb(err, body);
+    var json = JSON.parse(body);
+    cb(err, json);
+
+
+    for (var i in json) {
+      console.log(json[i]["avatar_url"])
+    }
+  });
 
 }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+});
