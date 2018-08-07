aa4c37acf0d7be5941e19591ba318c73008bda28
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-07 02:35:15 +0000

    add stream

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 //Node-style callback functions expect their first argument to be a placeholder for any errors that may have occurred, and the subsequent argument(s) are results being passed to the callback.
 function getRepoContributors(repoOwner, repoName, cb) { //need callback to handle the asynchronous nature of results that are to be returned from getRepoContributors.
@@ -21,8 +22,21 @@ function getRepoContributors(repoOwner, repoName, cb) { //need callback to handl
       console.log(json[i]["avatar_url"])
     }
   });
+}
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function (err) {
+           throw err;
+         })
+         .on('response', function (response) {
+          console.log('Response Status Code: ', response.statusCode);
+         })
+        .pipe(fs.createWriteStream(filePath));
 
 }
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   // console.log("Errors:", err);
